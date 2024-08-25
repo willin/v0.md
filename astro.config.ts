4aa58052ef97220716ba32f7a286d36a3c35c296
Willin Wang <i@v0.md>
2024-08-25 22:11:05 +0800

    feat: :sparkles: i18n

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { filterSitemapByDefaultLocale, i18n } from 'astro-i18n-aut/integration';
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
-import mdx from "@astrojs/mdx";
+import { defaultLocale, locales } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,19 @@ export default defineConfig({
   build: {
     format: 'directory'
   },
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    i18n({
+      locales,
+      defaultLocale
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale })
+    })
+  ]
 });
