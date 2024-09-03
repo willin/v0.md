f7210392c49e270aae39012eb6469a1c232279e5
Willin Wang <i@v0.md>
2024-09-03 20:19:31 +0800

    feat: :sparkles: init post layout

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,17 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeComponents from 'rehype-components';
+import rehypeSlug from 'rehype-slug';
+import remarkDirective from 'remark-directive';
+import remarkGithubAdmonitionsToDirectives from 'remark-github-admonitions-to-directives';
 import { defaultLocale, locales } from './src/consts';
+import { AdmonitionComponent } from './src/plugins/rehype-component-admonition.mjs';
+import { GithubCardComponent } from './src/plugins/rehype-component-github-card.mjs';
+import { parseDirectiveNode } from './src/plugins/remark-directive-rehype';
+import { remarkExcerpt } from './src/plugins/remark-excerpt';
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,6 +47,54 @@ export default defineConfig({
       }
     })
   ],
+  markdown: {
+    remarkPlugins: [
+      remarkReadingTime,
+      remarkExcerpt,
+      remarkGithubAdmonitionsToDirectives,
+      remarkDirective,
+      parseDirectiveNode
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeComponents,
+        {
+          components: {
+            github: GithubCardComponent,
+            note: (x, y) => AdmonitionComponent(x, y, 'note'),
+            tip: (x, y) => AdmonitionComponent(x, y, 'tip'),
+            important: (x, y) => AdmonitionComponent(x, y, 'important'),
+            caution: (x, y) => AdmonitionComponent(x, y, 'caution'),
+            warning: (x, y) => AdmonitionComponent(x, y, 'warning')
+          }
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            className: ['anchor']
+          },
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: ['anchor-icon'],
+              'data-pagefind-ignore': true
+            },
+            children: [
+              {
+                type: 'text',
+                value: '#'
+              }
+            ]
+          }
+        }
+      ]
+    ]
+  },
   devToolbar: {
     enabled: false
   }
